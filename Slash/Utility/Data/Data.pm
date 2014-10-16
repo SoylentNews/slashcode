d9242bb129b10fe75bc0714adea1a8fdd116481e
Thorsten M <github@thorzten.de>
2014-10-16 21:53:41 +0200

    Improve irony tag parsing with nested normal links - warning: contains still debug output

--- Slash/Utility/Data/Data.pm
+++ Slash/Utility/Data/Data.pm
@@ -1799,13 +1799,18 @@ sub processCustomTagsPost {
 
 	# thors: IRONY must be in approvedtags
 	if (grep /^irony$/i, @{$constants->{approvedtags}}) {
+		#thors debug print "Preprocessed comment:" . $str;
 		my $irony_open = "<a href=\"http://en.wikipedia.org/wiki/Irony\" style=\"text-decoration: none\">";
 		my $irony_close = "</a>";
-
-		while(($before, $middle, $after) = $s =~ /(.*)<\s*irony\s*>(.+)<\s*\/?\s*irony\s*>(.*)/s) {
-			$middle =~ s/(<\s*a\s+.*<\s*\/\s*a\s*>)/<\/a>$1$irony_open/g;
-			$s = $before . $irony_open . $middle . $irony_close . $after;
+		my $before;
+		my $middle;
+		my $after;
+		while(($before, $middle, $after) = $str =~ /(.*)<\s*irony\s*>(.*[^\s]+.*)<\s*\/?\s*irony\s*>(.*)/s) {
+			$middle =~ s/(<\s*a\s+.*<\s*\/\s*a[^>]*>)/<\/a>$1$irony_open/g;
+			$middle =~ s/[\n\r]//g;
+			$str = $before . $irony_open . $middle . $irony_close . $after;
 		}
+		#thors debug print "Preprocessed comment after irony:" . $str;
 	}
 
 	# just fix the whitespace for blockquote to something that looks
